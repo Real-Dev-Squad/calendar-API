4ec7526226ccc872cfca344b745a563defe31abf
Ankur Narkhede <ankurnarkhede@gmail.com>
2022-12-13 09:12:13 +0530

    Added nyc and mocha config

--- test/integration/health.test.ts
+++ test/integration/health.test.ts
@@ -5,11 +5,12 @@ chai.use(chaiHttp);
 
 import app from "../../server"
 
-describe("health", function () {
+describe("GET /health", function () {
   it("should return appropriate response from the healthcheck API", function (done) {
     chai
       .request(app)
-      .get("/health")
+      .get("/api/v1/health")
+      // .get("/api/v1/health")
       .end((err, res) => {
         if (err) {
           return done(err);
