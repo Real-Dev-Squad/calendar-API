d5c723bf9b8533824a68e82c3349d5613598a0c4
Prakash <om.pcprakash@gmail.com>
2022-12-29 13:16:05 +0530

    add validation

--- routes/events.ts
+++ routes/events.ts
@@ -2,14 +2,23 @@ import { Router } from "express";
 import { getCalendarEvents, getEvents, postEvent } from "../controllers/events";
 import authenticate from "../middlewares/authenticate";
 import { validate } from "../middlewares/validators/validator";
-import { postEventSchema } from "../middlewares/validators/zod-schemas/events";
+import {
+  getCalenderEventSchema,
+  getEventSchema,
+  postEventSchema,
+} from "../middlewares/validators/zod-schemas/events";
 
 const router = Router();
 
 /* eslint-disable @typescript-eslint/no-misused-promises */
 router.post("/", authenticate, validate(postEventSchema), postEvent);
-router.get("/:eventId", authenticate, getEvents);
-router.get("/calendar/:calendarId", authenticate, getCalendarEvents);
+router.get("/:eventId", authenticate, validate(getEventSchema), getEvents);
+router.get(
+  "/calendar/:calendarId",
+  authenticate,
+  validate(getCalenderEventSchema),
+  getCalendarEvents
+);
 /* eslint-enable @typescript-eslint/no-misused-promises */
 
 export default router;
