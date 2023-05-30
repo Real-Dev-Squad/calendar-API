1bbeeaa9f4a2e9ef4082c14d7f988bc8145d10c9
ritikjaiswal75 <ritik12103032000@gmail.com>
2023-05-30 23:28:04 +0530

    add tests for users self api

---
+++ test/integration/users.test.ts
@@ -0,0 +1,39 @@
+import chai from 'chai';
+
+import app from '../../server';
+import { generateAuthToken } from '../../services/authService';
+const { expect } = chai;
+
+describe('Integration | Route | /users/self', function () {
+
+  it('Get | /user/self | Error', function (done) {
+    chai
+      .request(app)
+      .get('/api/v1/users/self')
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        expect(res).to.have.status(401);
+        expect(res.body.error.message).equal('Unauthenticated User');
+        expect(res.body.error.statusCode).equal(401);
+        expect(res.body.error.error).equal('Unauthorized');
+        return done();
+      });
+  });
+
+  it.skip('GET | /user/self | Success', function (done) {
+    const jwt = generateAuthToken({ userId: 12524395 });
+    const cookieName = config.get('userAccessToken.cookieName');
+    chai
+      .request(app)
+      .get('/api/v1/users/self')
+      .set('Cookie', `${cookieName}=${jwt}`)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        expect(res).to.have.status(200);
+      });
+  });
+});
