a5a8b57a51d3b6706bfc3f506ca610a03dc7e880
Prakash <om.pcprakash@gmail.com>
2023-10-12 19:14:46 +0530

    add env to cookie name

--- config/default.ts
+++ config/default.ts
@@ -20,7 +20,7 @@ export default {
   },
 
   userAccessToken: {
-    cookieName: `rcal-session`,
+    cookieName: `rcal-session-${NODE_ENV}`,
     cookieDomain: '<cookie-domain>',
     ttl: 30 * 24 * 60 * 60, // in seconds
     refreshTtl: 180 * 24 * 60 * 60, // in seconds
